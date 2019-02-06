a00c3b0a1ba5fb139fa6a5529d0bf95cdb7ca311
lilia <liliaborrazas14@gmail.com>
2019-02-06 19:46:39 +0100

    create folder service and file reposService

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import { fetchRepos } from './services/reposService';
+
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      repos: []
+    };
+
+    this.getRepos();
+  }
+
+  getRepos(){
+    fetchRepos()
+     .then(data => {
+       this.setState({
+         repos: data
+       })
+     });
+  }
   render() {
     return (
       <div className="app">
