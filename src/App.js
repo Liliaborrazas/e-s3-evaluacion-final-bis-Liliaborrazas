8e51d0686cbf1237b1e3ce6c37e98dd3e85257f1
lilia <liliaborrazas14@gmail.com>
2019-02-06 20:01:43 +0100

    get list repos

--- src/App.js
+++ src/App.js
@@ -27,7 +27,19 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-      :)
+      <ul className="app__list">
+      {this.state.repos.map(item =>{
+        return(
+        <li className="app__list-item">
+          <div className="repos">
+           <div className="repos__name">{item.name}</div>
+           <p className="repos__info">{item.description}</p>
+           <p className="repos__language">{item.language}</p>
+          </div>
+        </li>
+               )
+      })}
+      </ul>
         
       </div>
     );
